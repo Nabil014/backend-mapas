73e097d8d2b16ef060828e81d978e5241ea73ee6
Joshua_Arfa <soondevjosh@gmail.com>
2023-05-04 14:16:50 -0600

    feat: adding register component

--- apps/web-access-map/src/containers/auth/LoginContainer.jsx
+++ apps/web-access-map/src/containers/auth/LoginContainer.jsx
@@ -18,10 +18,10 @@ export default function LoginContainer () {
   return (
     <div className='flex flex-col-reverse justify-center items-center gap-80 h-screen font-poppi   md:flex-row'>
       <div className='justify-center items-center h-44 md:w-screen md:items-stretch md:basis-1/2 lg:basis-1/2'>
-        <h1 className='text-blue-400 -mt-[110%] text-5xl text-center -ml-[7.6%]  md:-mt-[37%] md:mb-7 md:ml-[50%] md:text-6xl lg:text-7xl lg:mr-[60] 2xl:ml-[45%]'>AccesMap</h1>
-        <section className='basi justify-center items-center p-5 ml-[15.5%] text-2xl w-[80%]  md:w-[130%] md:ml-[52%] md:self-center lg:ml-[36%] 2xl:ml-[14.4%]'>
-          <h2 className='text-center text-4xl -ml-[11%] mr-[30%] md:ml-[1.5%] md:text-5xl lg:text-5xl lg:ml-[20%] 2xl:ml-[25%]'>Welcome!</h2>
-          <span className='text-gray-600 text-sm -ml-[23%] text-center md:-ml-[23%] md:text-2xl  lg:text-lg lg:ml-[12%] xl:ml-[13%] xl:text-gray-600 2xl:ml-[20.5%]'>Sign by entering your information</span>
+        <h1 className='text-sky-500 -mt-[85%] text-5xl text-center -ml-[1.6%]  md:-mt-[37%] md:mb-7 md:ml-[50%] md:text-6xl lg:text-7xl lg:mr-[60] 2xl:ml-[45%]'>AccesMap</h1>
+        <section className='basi justify-center items-center p-5 ml-[17.5%] text-2xl w-[80%]  md:w-[130%] md:ml-[52%] md:self-center lg:ml-[36%] 2xl:ml-[14.4%]'>
+          <h2 className='text-center text-4xl -ml-[3%] mr-[30%] md:ml-[1.5%] md:text-5xl lg:text-5xl lg:ml-[20%] 2xl:ml-[25%]'>Welcome!</h2>
+          <span className='text-gray-600 text-sm -ml-[11%] text-center md:-ml-[23%] md:text-2xl  lg:text-lg lg:ml-[12%] xl:ml-[13%] xl:text-gray-600 2xl:ml-[20.5%]'>Sign by entering your information</span>
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className='mt-5'>
               <input
@@ -34,8 +34,8 @@ export default function LoginContainer () {
                 autoFocus
                 className='bg-gray-100 p-1 rounded-lg text-lg ml-[-16.7%] w-[108.7%] md:-ml-[20%] md:p-2 lg:ml-[-10%] xl:ml-[-9%] 2xl:p-3 2xl:w-[80%] 2xl:ml-[7.8%]'
               />
-              {errors.username?.type === 'required' && (<p className='text-sm text-red-600 -ml-[20%] md:ml-[-18%] md:text-lg xl:ml-[-7%] xl:text-lg 2xl:ml-[9%] 2xl:text-2xl'>Username is required</p>)}
-              {errors.username?.type === 'maxLength' && (<p className='text-sm text-red-600 -ml-[19.5%] md:ml-[-18%] md:text-lg xl:ml-[-7%] xl:text-lg 2xl:ml-[9%] 2xl:text-2xl'>Username must have at least 10 characteres</p>)}
+              {errors.username?.type === 'required' && (<p className='text-sm text-red-600 -ml-[17%] md:-ml-[10.2%] md:text-lg lg:-ml-[10%] xl:ml-[-7%] xl:text-lg 2xl:ml-[9%] 2xl:text-2xl'>Username is required</p>)}
+              {errors.username?.type === 'maxLength' && (<p className='text-sm text-red-600 -ml-[17.5%] md:-ml-[10.2%] md:text-lg xl:ml-[-8%] xl:text-lg 2xl:ml-[9%] 2xl:text-2xl'>Username must have at least 10 characteres</p>)}
             </div>
             <div className='mt-1'>
               <input
@@ -46,8 +46,9 @@ export default function LoginContainer () {
                 placeholder='Password'
                 className='bg-gray-100 p-1 mt-2 rounded-lg text-lg ml-[-16.7%] w-[108.7%] md:-ml-[20%] md:p-2 lg:ml-[-10%] xl:ml-[-9%] 2xl:p-3 2xl:w-[80%] 2xl:ml-[7.8%]'
               />
-              {errors.password?.type === 'required' && (<p className='text-sm text-red-600 -ml-[17%] md:ml-[-18%] md:text-lg xl:ml-[-7%] xl:text-lg 2xl:ml-[9%] 2xl:text-2xl'>Password is required</p>)}
+              {errors.password?.type === 'required' && (<p className='text-sm text-red-600 -ml-[17%] md:-ml-[10.2%] md:text-lg xl:-ml-[8%] xl:text-lg 2xl:ml-[9%] 2xl:text-2xl'>Password is required</p>)}
             </div>
+            <p className='font-medium text-gray-600 text-sm -ml-[19.3%] mt-2 sm:-ml-10 md:text-base md:-ml-[20%] md:mt-2 xl:text-base lg:-ml-[10%] xl:-ml-[8%] xl:mt-2 2xl:text-lg 2xl:ml-[8%] 2xl:mt-2'>Need an AccessMap account? Create an account</p>
             <ButtonLogin />
           </form>
         </section>
--- apps/web-access-map/src/router/RouterPrincipal.jsx
+++ apps/web-access-map/src/router/RouterPrincipal.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { LoginPage } from '../pages/auth/LoginPage'
+import { RegisterPage } from '../pages/auth/RegisterPage'
 
 export const RouterPrincipal = () => {
   return (
     <>
       <Routes>
+        <Route path='/register' element={<RegisterPage />} />
         <Route path='/login' element={<LoginPage />} />
 
         <Route path='/*' element={<Navigate to='/login' />} />
