b0cce7f759ba39e07e025a22653b55692dd55148
franz <gavino_10@hotmail.es>
2023-05-04 17:16:20 -0500

    feat:maquetando ......

---
+++ apps/web-access-map/src/api/apiMapas.js
@@ -0,0 +1,16 @@
+/* import axios from 'axios'
+
+const apiMapas = axios.create({
+  baseURL: import.meta.env.VITE_API_APP_MAPAS
+})
+
+apiMapas.interceptors.request.use((config) => {
+  config.headers = {
+    ...config.headers,
+    Authorization: 'bearer ' + localStorage.getItem('token'),
+  };
+
+  return config
+});
+
+export default apiMapas */
--- apps/web-access-map/src/pages/navbar/NavBarPage.jsx
+++ apps/web-access-map/src/pages/navbar/NavBarPage.jsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
 import { FaMap, FaComments, FaEdit, FaCog } from 'react-icons/fa'
 import { NavLink } from 'react-router-dom'
 
 export const NavBarPage = () => {
+  const [activeIndex, setActiveIndex] = useState(null)
+
+  const clickClass = (index) => {
+    setActiveIndex(index)
+  }
+
+  const classFunction = (index) => {
+    if (index === activeIndex) {
+      return 'nav-item nav-link flex items-center space-x-4 bg-gradient-to-r from-sky-600 to-cyan-400 px-4 py-3 text-white'
+    } else {
+      return 'nav-item nav-link group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600'
+    }
+  }
   return (
     <div className=' min-h-screen bg-gray-100'>
       <div className='sidebar min-h-screen w-[3.35rem] overflow-hidden border-r hover:w-56 hover:bg-white hover:shadow-lg'>
@@ -13,25 +27,25 @@ export const NavBarPage = () => {
             </div>
             <ul className='mt-6 space-y-2 tracking-wide'>
               <li className='min-w-max'>
-                <NavLink className='nav-item nav-link relative flex items-center space-x-4 bg-gradient-to-r from-sky-600 to-cyan-400 px-4 py-3 text-white' to='/mapa'>
-                  <FaMap className='-ml-1 h-6 w-6' />
+                <NavLink className={classFunction(1)} to='/mapa' onClick={() => clickClass(1)}>
+                  <FaMap className='h-5 w-5 fill-current group-hover:text-cyan-300' />
                   <span className='-mr-1 font-medium'>MAPA</span>
                 </NavLink>
               </li>
               <li className='min-w-max'>
-                <NavLink className='nav-item nav-link group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600' to='/feddback'>
+                <NavLink className={classFunction(2)} to='/feddback' onClick={() => clickClass(2)}>
                   <FaComments className='h-5 w-5 fill-current group-hover:text-cyan-300' />
                   <span className='-mr-1 font-medium'>FEDDBACK</span>
                 </NavLink>
               </li>
             </ul>
           </div>
-          <div className='w-max -mb-3'>
-            <NavLink className='nav-item nav-link group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600' to='/editperfil'>
+          <div className='min-w-max '>
+            <NavLink className={classFunction(3)} to='/editperfil' onClick={() => clickClass(3)}>
               <FaEdit className='h-5 w-5 fill-current group-hover:text-cyan-300' />
               <span className='-mr-1 font-medium'>Editar Perfil</span>
             </NavLink>
-            <NavLink className='nav-item nav-link group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600' to='/settings'>
+            <NavLink className={classFunction(4)} to='/settings' onClick={() => clickClass(4)}>
               <FaCog className='h-5 w-5 fill-current group-hover:text-cyan-300' />
               <span className='-mr-1 font-medium'>Settings</span>
             </NavLink>
