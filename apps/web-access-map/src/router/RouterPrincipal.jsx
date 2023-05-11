81f40d58a9e21f7258af9669930d556896512ddd
franz <gavino_10@hotmail.es>
2023-05-11 15:18:27 -0500

    feat:terminando login

--- apps/web-access-map/src/router/RouterPrincipal.jsx
+++ apps/web-access-map/src/router/RouterPrincipal.jsx
@@ -4,15 +4,20 @@ import { LoginPage } from '../pages/auth/LoginPage'
 import { NavBarPage } from '../pages/navbar/NavBarPage'
 import { MapaPage } from '../pages/mapa/MapaPage'
 import { RegisterPage } from '../pages/auth/RegisterPage'
+import { useSelector } from 'react-redux'
 
 export const RouterPrincipal = () => {
+  const {status}=useSelector((state) => state.auth)
+  const isLoged = status === 'authenticated'
+
   return (
     <>
       <Routes>
         <Route path='/' element={<MapaPage />} />
         <Route path='/register' element={<RegisterPage />} />
         <Route path='/login' element={<LoginPage />} />
-        <Route path='/main' element={<NavBarPage />} />
+
+        <Route path='/main' element={isLoged ? <NavBarPage /> : <LoginPage />} />
         <Route path='/mapa' element={<MapaPage />} />
         <Route path='/feddback' element={<NavBarPage />} />
         <Route path='/editperfil' element={<NavBarPage />} />
